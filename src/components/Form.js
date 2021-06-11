0b44a02eed87ce820472679f768946225ce0c639
jyotiprakash-m <teachmeonline365@gmail.com>
2021-06-11 18:56:21 +0530

    email validation complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 import Avatar from '@material-ui/core/Avatar';
+import validator from 'validator'
 
 function Form({ setDisplayForm, displayForm }) {
+    const [email, setEmail] = useState("")
+    const [mobile, setMobile] = useState("")
+    const [emailError, setEmailError] = useState(null)
+    const [mobileError, setMobileError] = useState(null)
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        var pattern = new RegExp(/^[0-9\b]+$/);
+        if (!validator.isEmail(email)) {
+            setEmailError('Please enter a valid email')
+        }
+        if (!pattern.test(mobile)) {
+            setMobileError("Please enter a valid number")
+        }
+    }
+    useEffect(() => {
+        setMobileError(null)
+        setEmailError(null)
+    }, [email, mobile])
+
     return (
         <div className="form">
             <div className="form__contains">
@@ -23,34 +43,36 @@ function Form({ setDisplayForm, displayForm }) {
                 <div className="form__right">
                     <h3>Thank you so much for taking the time!</h3>
                     <p>Please provide the below details</p>
-                    <form>
+                    <form onSubmit={handleSubmitForm}>
                         <div className="form__input">
                             <p>First Name:</p>
-                            <input type="text" />
+                            <input type="text" placeholder="Jhon" />
                         </div>
                         <div className="form__input">
                             <p>Last Name:</p>
-                            <input type="text" />
+                            <input type="text" placeholder="Doe" />
                         </div>
                         <div className="form__input">
                             <p>Address:</p>
-                            <textarea rows="6" cols="80"></textarea>
+                            <textarea rows="6" cols="80" placeholder="Enter your full Postal Address"></textarea>
                         </div>
                         <div className="form__input">
                             <p>Search:</p>
                             <div className="searchInput">
-                                <input type="text" />
+                                <input type="text" placeholder="India" />
                                 <SearchIcon />
                             </div>
                         </div>
                         <div className="form__input">
                             <p>Email ID:</p>
-                            <input type="text" />
+                            <input type="text" placeholder="example@sample.com" onChange={(e) => setEmail(e.target.value)} />
+                            {emailError && <span className="error">{emailError}</span>}
                         </div>
                         <div className="form__input">
                             <p>Phone Number:</p>
-                            <input type="text" className="countryCode" />
-                            <input type="number" className="phoneNumber" />
+                            <input type="text" className="countryCode" placeholder="+91" />
+                            <input type="number" className="phoneNumber" placeholder="123456789" onChange={(e) => setMobile(e.target.value)} />
+                            {mobileError && <span className="error">{mobileError}</span>}
                         </div>
                         <div className="form__submit">
                             <input type="submit" value="Submit Feeedback" />
